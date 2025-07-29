a2b7f41c542480da6bb590645c8924ddb2e09002
Pavan Bhalerao <148614171+pavanbhalerao01@users.noreply.github.com>
2025-07-29 12:43:48 +0530

    Update download_model.py

--- model/download_model.py
+++ model/download_model.py
@@ -1,16 +1,12 @@
-# download_model.py
 from sentence_transformers import SentenceTransformer
 import os
 
-# Specify the model name and the local directory to save it to
 model_name = 'all-MiniLM-L6-v2'
 save_path = './model'
 
-# Create the directory if it doesn't exist
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-# Download and save the model
 print(f"Downloading model '{model_name}' to '{save_path}'...")
 model = SentenceTransformer(model_name)
 model.save(save_path)
